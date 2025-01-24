5eda84acaa518d3b00ee2e4b5db6064307afc94c
BetterLordWilliam <willjordan05@icloud.com>
2025-01-24 13:10:41 -0800

    WO: Added a vector 3 struct. Tried to break the program by not using fixed keyword, need to understand the fixed keyword better I think.

--- EvenMorePointers.cs
+++ EvenMorePointers.cs
@@ -3,34 +3,41 @@ using System.Runtime.CompilerServices;
 namespace UnsafeTesting
 {
 
-/// <summary>
-/// Vector3: represents a 3D Vector.
-/// </summary>
-/// <param name="x"></param>
-/// <param name="y"></param>
-/// <param name="z"></param>
-class Vector3 (int x, int y, int z)
-{
-    int x = x, y = y, z = z;
-
-    public override string ToString()
+    /// <summary>
+    /// Vector3: represents a 3D Vector.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="z"></param>
+    struct Vector3S(int x, int y, int z)
     {
-        return $"{x}, {y}, {z}";
+        int x = x, y = y, z = z;
+
+        public override string ToString()
+        {
+            return $"{x}, {y}, {z}";
+        }
     }
-}
 
-/// <summary>
-/// Another unsafe class!
-/// </summary>
-unsafe class UnsafeClass1
-{
-    public Vector3* MyVector3 { get; }
-
-    public UnsafeClass1(Vector3 aVector3)
+    class Vector3C(int x, int y, int z)
     {
-        MyVector3 = &aVector3;  // erroneous, since Vector3 is a moveable type
-                                // garbage collectable, and we are in unsafe code!
+        int x = x, y = y, z = z;
+        public override string ToString()
+        {
+            return $"{x}, {y}, {z}";
+        }
     }
-}
 
+    /// <summary>
+    /// Another unsafe class!
+    /// </summary>
+    unsafe class UnsafeClass1
+    {
+        public Vector3S* MyVector3 { get; }
+
+        public UnsafeClass1(Vector3S* aVector3)
+        {
+            MyVector3 = aVector3;
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,37 +1,46 @@
 ﻿using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace UnsafeTesting
 {
-        // int willint;
-        // unsafe
-        // {
-        //     int* iptr = &willint;
-        //     *iptr = 10;
-        // }
-        // Console.WriteLine(willint);
-        
-        // UnsafeClass us1 = new UnsafeClass(1,2);
-        // us1.ShowMyStuff();
+    // int willint;
+    // unsafe
+    // {
+    //     int* iptr = &willint;
+    //     *iptr = 10;
+    // }
+    // Console.WriteLine(willint);
 
-public static class Program
-{
-    /// <summary>
-    /// Program entry.
-    /// </summary>
-    /// <param name="args"></param>
-    public static void Main(string[] args)
+    // UnsafeClass us1 = new UnsafeClass(1,2);
+    // us1.ShowMyStuff();
+
+    public static class Program
     {
-        Vector3 v3 = new (10,15,20);
-        UnsafeClass1 usc1 = new (v3);
-
-        Thread.Sleep(10);
-
-        // Can write unsafe blocks too btw, just means code in the scope of the block is unsafe context
-        unsafe
+        /// <summary>
+        /// Program entry.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
         {
-            // Somehow, somewhere else, the value was destroyed or moved.
-            Console.WriteLine($"Value of the vector = " + *(usc1.MyVector3));
+            Vector3S v3 = new(10, 15, 20);
+
+            //unsafe
+            //{
+            //    fixed (Vector3* v3pf = &v3) // fixed keyword is used to pin a pointer to a managed object
+            //    {
+            //        usc1 = new UnsafeClass1(*v3pf);
+            //    }
+            //}
+
+
+            // Can write unsafe blocks too btw, just means code in the scope of the block is unsafe context
+            unsafe
+            {
+                Vector3S* v3p = &v3;
+                Thread.Sleep(10);
+                UnsafeClass1 usc1 = new(v3p);
+                Console.WriteLine($"Value of the vector = " + *(usc1.MyVector3));
+            }
         }
     }
-}
 }
