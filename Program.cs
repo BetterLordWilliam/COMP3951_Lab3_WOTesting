4df56df0a7e096e9cf86a44cda639aff31a5913f
LordWilliam <willjordan05@icloud.com>
2025-01-23 19:15:24 -0800

    WO: Initial commit.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace UnsafeTesting
+{
+    public static class Program
+    {
+        /// <summary>
+        /// Program entry.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Will Otterbein!!");
+        }
+    }
+}
